0f22b5791f20d3c6b348af030dc626a2e8525147
Jimmy MUNOZ <jmunoz@adilibre.fr>
2021-07-10 19:24:13 +0200

    feat: add sign - signup

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,15 +1,32 @@
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
+import * as bcrypt from 'bcrypt';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto
+
+    const salt = await bcrypt.genSalt()
+    const hashedPassword = await bcrypt.hash(password, salt)
+
     const user = this.create({
       username,
-      password,
+      password: hashedPassword,
     })
-    await this.save(user)
+
+    try {
+      await this.save(user)
+    } catch (error) {
+      console.error('UserRepository', error.code);
+      // Duplication username
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists')
+      }
+      else {
+        throw new InternalServerErrorException()
+      }
+    }
   }
 }
